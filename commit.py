5ae82645daa6c0b9f2851637ce17d74f09a138a1
Mukul Jain <mukuljainagro@gmail.com>
2017-06-28 20:11:11 +0530

    Commit Message Here ascassadas

--- commit.py
+++ commit.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import os
-f = open('test1.html','a')
 for i in range(100):
+	f = open('test1.html','a')
 	f.write('mukul')
 	os.system('export GIT_COMMITTER_DATE="2017-05-28 20:11:11"')
 	os.system('export GIT_AUTHOR_DATE="2017-06-28 20:11:11"')
 	os.system('git add .')
 	os.system("git commit -am 'Commit Message Here ascassadas'")
 	os.system('git push -u origin master')
+	f.close()
