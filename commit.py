ef317739bf8d4f1439e68434a960d42316188c86
Mukul Jain <mukuljainagro@gmail.com>
2017-10-04 15:47:20 +0530

    test commit today

--- commit.py
+++ commit.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import os
-var = 1
+var = 01
+var = str(var)
 for i in range(100):
 	f = open('test1.html','a')
 	f.write('mukul')
-	if(var>=30):
-		var = 1
-	if (var<=9):
-		aq = str(var)
-		a= "0"+str(var)
-	var = int(var)
+	if var<10:
+		var = "0"+str(var)
 	dat = "2017-07-"+ str(var)+" "+"20:11:11"
 	aas = 'export GIT_COMMITTER_DATE='+'"'+dat+'"'
 	aaas = 'export GIT_AUTHOR_DATE='+'"'+dat+'"'
@@ -18,7 +15,7 @@ for i in range(100):
 	print aaas
 	os.system(aas)
 	os.system(aaas)
-	var = var+1
+	(var) = int(var)+1
 	os.system('git add .')
 	os.system("git commit -am 'Commit Message Here ascassadas'")
 	os.system('git push -u origin master')
---
+++ fincommit.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+import os
+import ConfigParser
+
+print "Enter date of format 2017-07-09"
+date = raw_input()
+
+print "Enter the time of format 20:11:11"
+time = raw_input()
+
+print "Enter your name"
+name = raw_input()
+if(name=="Mukul Jain"):
+	email = "mukuljainagro@gmail.com"
+
+print "Enter commit message"
+m = raw_input()
+os.system("git config --gloabl user.name" +name)
+os.system("git config --global user.email" +email)
+
+final = date+" "+time
+aas = 'export GIT_COMMITTER_DATE='+'"'+final+'"'
+aaas = 'export GIT_AUTHOR_DATE='+'"'+final+'"'
+print aas
+print aaas
+os.system(aas)
+os.system(aaas)
+os.system('git add .')
+os.system("git commit -am "+"'"+m+"'")
+os.system('git push -u origin master')
